b75ff439ef891e999823c608cbec7db3813f7454
CHENYANG <li.chenyang1990@gmail.com>
2015-10-11 23:18:02 +0800

    Course Project ExDataAnalysis

---
+++ plot1.R
@@ -0,0 +1,36 @@
+#... Plot1.R... 
+
+# ---- Objective 1: Get the subset of data
+library(plyr)
+library(dplyr)
+hh_pow <- read.table("household_power_consumption.txt", 
+                     header = TRUE, sep = ";", na.strings = "?")
+hh_pw1 <- filter(hh_pow, Date == c("1/2/2007", "2/2/2007"))
+#..Here i got a warning message--
+# Warning messages:
+#   1: In is.na(e1) | is.na(e2) :
+#   longer object length is not a multiple of shorter object length
+# 2: In `==.default`(c(326L, 326L, 326L, 
+#                     326L, 326L, 326L, 326L, 326L,  :
+#                   longer object length is not a multiple of shorter object length
+rm(hh_pow)
+
+# When I was doing the assignment, I referred to the thread below. 
+# https://class.coursera.org/exdata-033/forum/thread?thread_id=15
+# credit has to be given to Micheal Jackson who had shown his methods in that thread. 
+
+hh_pw1$Date_Time <- paste(hh_pw1$Date, hh_pw1$Time, sep = " ")
+hh_pw1$Date_Time <- strptime(hh_pw1$Date_Time, format = "%d/%m/%Y %H:%M:%S")
+
+#hh_pw1 <- select(hh_pw1, -(Date:Time))
+
+#----Requirements for Graphs-------
+# Construct the plot and save it to a PNG file with 
+# a width of 480 pixels and a height of 480 pixels.
+png("plot1.png", bg = "transparent", height = 480, width = 480)
+hist(hh_pw1$Global_active_power, col = "dark red", 
+     xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
+dev.off()
+
+
+
---
+++ plot4.R
@@ -0,0 +1,53 @@
+#... Plot1.R... 
+
+# ---- Objective 1: Get the subset of data
+library(plyr)
+library(dplyr)
+hh_pow <- read.table("household_power_consumption.txt", 
+                     header = TRUE, sep = ";", na.strings = "?")
+hh_pw1 <- filter(hh_pow, Date == c("1/2/2007", "2/2/2007"))
+#..Here i got a warning message--
+# Warning messages:
+#   1: In is.na(e1) | is.na(e2) :
+#   longer object length is not a multiple of shorter object length
+# 2: In `==.default`(c(326L, 326L, 326L, 
+#                     326L, 326L, 326L, 326L, 326L,  :
+#                   longer object length is not a multiple of shorter object length
+rm(hh_pow)
+
+# When I was doing the assignment, I referred to the thread below. 
+# https://class.coursera.org/exdata-033/forum/thread?thread_id=15
+# credit has to be given to Micheal Jackson who had shown his methods in that thread. 
+
+
+hh_pw1$Date_Time <- paste(hh_pw1$Date, hh_pw1$Time, sep = " ")
+hh_pw1$Date_Time <- strptime(hh_pw1$Date_Time, format = "%d/%m/%Y %H:%M:%S")
+
+hh_pw1$Date <- as.Date(hh_pw1$Date)
+#hh_pw1 <- select(hh_pw1, -(Date:Time))
+
+#----Requirements for Graphs-------
+# Construct the plot and save it to a PNG file with 
+# a width of 480 pixels and a height of 480 pixels.
+png("plot4.png", bg = "transparent", height = 480, width = 480)
+par(mfrow = c(2, 2))
+par(mar = c(4, 4, 1, 1))
+with(hh_pw1, plot(Date_Time, Global_active_power, ylab = "Global Active Power", 
+                  xlab = "", type = "l"))
+with(hh_pw1, plot(Date_Time, Voltage, ylab = "", xlab = "datetime", 
+                  type = "l"))
+
+plot(hh_pw1$Date_Time, hh_pw1$Sub_metering_1, col = "black", type = "l",
+     ylab = "Energy Sub Metering", xlab = "")
+lines(hh_pw1$Date_Time, hh_pw1$Sub_metering_2, col = "red")
+lines(hh_pw1$Date_Time, hh_pw1$Sub_metering_3, col = "blue")
+legend("topright", pch = "_", col = c("black", "red", "blue"), 
+       legend = c("Sub metering 1", "Sub metering 2", "Sub metering 3"))
+
+with(hh_pw1, plot(Date_Time, Global_reactive_power, type = "l", 
+                  ylab = "Global_reactive_power", xlab = "datetime"))
+
+dev.off()
+
+
+
